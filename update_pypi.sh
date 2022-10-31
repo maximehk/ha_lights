4fb833ee9bd2f03c242e453df38c037f4de9548d
Max <max@noreply.com>
2022-10-31 14:00:54 +0100

    Use secrets from 1password

--- update_pypi.sh
+++ update_pypi.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-python setup.py bdist_wheel
-twine upload --skip-existing dist/*   
+
+update_pypi() {
+	local username=$(op items get hwgpo3mrfdnvxothu7z6cc3pau --format json --field=username | jq -r '.value')
+	local password=$(op items get hwgpo3mrfdnvxothu7z6cc3pau --format json --field=password | jq -r '.value')
+
+	python setup.py bdist_wheel
+	twine upload -u "${username}" -p "${password}" --verbose --skip-existing dist/*
+}
+
+update_pypi
